68ea7ba06d4bd63808be975264de13138ebe8765
Desteveco <destevezcounago@gmail.com>
2025-01-09 11:00:57 +0100

    refactor: ones refactor

--- src/yatzy.py
+++ src/yatzy.py
@@ -13,20 +13,10 @@ class Yatzy:
 
     @staticmethod
     def ones(d1, d2, d3, d4, d5):
-        sum = 0
-        if (d1 == 1):
-            sum += 1
-        if (d2 == 1):
-            sum += 1
-        if (d3 == 1):
-            sum += 1
-        if (d4 == 1):
-            sum += 1
-        if (d5 == 1):
-            sum += 1
-
-        return sum
-
+        
+        ones_score = sum(1 for i in [d1, d2, d3, d4, d5] if i == 1)
+        return ones_score
+        
     @staticmethod
     def twos(d1, d2, d3, d4, d5):
         sum = 0
