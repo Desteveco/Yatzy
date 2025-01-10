878b0bf20a0eb4010c772f8aece7f62a34655d86
GabrielgsdCIUwU <holagabriel321@gmail.com>
2025-01-10 12:51:49 +0100

    refactor(test): :recycle: refactor test fours

--- test/test_fours.py
+++ test/test_fours.py
@@ -1,19 +1,8 @@
 import pytest
 from src.yatzy import Yatzy
 
-@pytest.fixture
-def inyector():
-    # es el setup de unittest o de JUnit
-    tirada = Yatzy(4, 5, 6, 4, 5)
-    return tirada
-
-
-def test_fours(inyector):
-    '''
-    The player scores the sum of the dice that reads four
-    '''
-    # es necesario un objeto de tipo Yatzy ya creado
-    valorEsperado = 8
-    # No puedo testear con fixtures = inyeccion de dependencias
-    # los metodos estaticos como chance()
-    assert valorEsperado == inyector.fours()
+@pytest.mark.fours
+def test_fours_test():
+    assert 12 == Yatzy.fours(4, 4, 4, 5, 5)
+    assert 8 == Yatzy.fours(4, 4, 5, 5, 5)
+    assert 4 == Yatzy.fours(4, 5, 5, 5, 5)
