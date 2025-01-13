9d03a9c12b414d61d09de798ddcc5686b406f3ad
Desteveco <destevezcounago@gmail.com>
2025-01-13 14:15:19 +0100

    test(test): refactor full_house tests

--- test/test_fullhouse.py
+++ test/test_fullhouse.py
@@ -2,8 +2,12 @@ import pytest
 from src.yatzy import Yatzy
 
 @pytest.mark.full
-def test_fullHouse():
-    assert 8 == Yatzy.fullHouse(1, 1, 2, 2, 2)
-    assert 0 == Yatzy.fullHouse(2, 2, 3, 3, 4)
-    assert 0 == Yatzy.fullHouse(4, 4, 4, 4, 4)
-    assert 0 == Yatzy.fullHouse(4, 4, 4, 1, 2)  
+def test_full_house():
+    assert 8 == Yatzy.full_house(1, 1, 2, 2, 2)
+    assert 0 == Yatzy.full_house(2, 2, 3, 3, 4)
+
+def test_full_house1():
+    assert 0 == Yatzy.full_house(4, 4, 4, 4, 4)
+ 
+def test_full_house2():
+    assert 0 == Yatzy.full_house(4, 4, 4, 1, 2) 
